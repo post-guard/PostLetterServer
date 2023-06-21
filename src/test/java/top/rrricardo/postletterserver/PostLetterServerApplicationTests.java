29d049a4a4ed495259cbe60886f718de48dee24f
jackfiled <xcrenchangjun@outlook.com>
2023-06-21 22:27:58 +0800

    fix: 默认测试运行失败

--- src/test/java/top/rrricardo/postletterserver/PostLetterServerApplicationTests.java
+++ src/test/java/top/rrricardo/postletterserver/PostLetterServerApplicationTests.java
@@ -3,9 +3,7 @@ package top.rrricardo.postletterserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PostLetterServerApplicationTests {
-
     @Test
     void contextLoads() {
     }
